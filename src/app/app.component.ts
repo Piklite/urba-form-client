1286675952bdeed5a573b86424dc490462766375
Grégory Da Silva <gregory.dasilva@operis.fr>
2022-02-16 11:26:39 +0100

    requete http test vers UrbaFormServer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'urba-form-client';
+  public error: string = '';
+  public typesDossier$: any = this.httpClient
+    .get('http://localhost:3000/types-dossier')
+    .pipe(
+      tap((res) => console.log(res)),
+      catchError((e) => {
+        console.log(e);
+        return (this.error = e.message);
+      })
+    );
+
+  public constructor(private httpClient: HttpClient) {}
 }
